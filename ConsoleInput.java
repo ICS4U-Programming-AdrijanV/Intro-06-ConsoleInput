d491ddd04fe159bdbc777c15d73fb93b09f70627
Adrijan Vranjkovic <adrijan.vranjkovic@stu.ocsb.ca>
2023-02-13 03:44:56 +0000

    added text

--- ConsoleInput.java
+++ ConsoleInput.java
@@ -7,9 +7,9 @@
 * @since   2023-02-10
 */
 
-//Importing
+// Importing
 import java.util.Scanner;
-
+// Making class
 public final class ConsoleInput {
 
     /**
@@ -18,6 +18,8 @@ public final class ConsoleInput {
      * @exception IllegalStateException Utility class.
      * @see IllegalStateException
      */
+     volume = 0
+     radiusDouble = 0
     private ConsoleInput() {
         throw new IllegalStateException("Utility class.");
     }
@@ -28,19 +30,26 @@ public final class ConsoleInput {
      * @param args Unused.
      */
     public static void main(String[] args) {
-        //Using scanner to get user input
+        // Using scanner to get user input
+        System.out.println("What is the radius in centimeters?");
         Scanner scanner = new Scanner(System.in);
         String userRadius = scanner.nextLine();
-        
-        //calculating
+        // Make user input a double.
         double radiusDouble = Double.parseDouble(userRadius);
+        // Check for valid input.
         if (radiusDouble < 0) {
             System.out.println("This is a negative value.");
-        }
-        double volume = ((3.0 / 4.0) * Math.PI) * Math.pow(radiusDouble, 3);
+        } else if (radiusDouble > 0) {
 
-        System.out.println("The total volume is " + (volume));
-        
+            // Calculate
+            double volume = ((4.0 / 3.0) * Math.PI) * Math.pow(radiusDouble, 3);
+            // Print volume.
+            System.out.println("The total volume is : "
+                 + String.format("%.2f", volume) + " cm^3.");
+        } else {
+            System.out.println("This not a possible input.");
+        }
+        // Close scanner.
         scanner.close();
     }
 }
